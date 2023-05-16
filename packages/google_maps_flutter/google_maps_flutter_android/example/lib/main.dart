5681c84504e9e6a856bda8570645c4af4cc6097f
Joonas Kerttula <joonas.kerttula@codemate.com>
2023-05-16 08:04:14 +0300

    Documentation fixes

--- packages/google_maps_flutter/google_maps_flutter_android/example/lib/main.dart
+++ packages/google_maps_flutter/google_maps_flutter_android/example/lib/main.dart
@@ -84,7 +84,8 @@ void main() {
 
 Completer<AndroidMapRenderer?>? _initializedRendererCompleter;
 
-/// Initializes map renderer to with `latest` renderer type.
+/// Initializes map renderer to the `latest` renderer type.
+/// 
 /// The renderer must be requested before creating GoogleMap instances,
 /// as the renderer can be initialized only once per application context.
 Future<AndroidMapRenderer?> initializeMapRenderer() async {
